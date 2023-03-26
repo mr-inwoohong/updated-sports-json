ca0f45f40a428caea9238e1d099cc1cae883d6d6
inweez <115917221+inweez@users.noreply.github.com>
2023-03-26 15:16:31 -0700

    initial commit - updated to pull in json format dictinary instead of list correct key/value for dict

--- mlb_batting_json.py
+++ mlb_batting_json.py
@@ -27,15 +27,17 @@ def mlb_batting_stats(url):
 
     data_rows = table.find('tbody').find_all('tr')
 
-    data = []
+
 
     data_dict = {}
     for row in data_rows:
         cols = row.find_all(['th', 'td'])
         cols = [col.text.strip() for col in cols]
         key = cols[0]  # assuming the key is in the first column
-        data_dict[key] = [{'Year': cols[1], 'Age': cols[2], 'League': cols[3], 'Games Played': cols[4], 'Plate Appearance': cols[5], 'At Bat': cols[6], 'Runs Scored': cols[7], 'Hits': cols[8], 'Doubles': cols[9], 'Triples': cols[10], 'Homeruns': cols[11], 'RBI': cols[12], 'Stolen Bases': cols[13], 'Caught Stealing': cols[14], 'BB/Walks': cols[15], 'Strikeouts': cols[16], 'At Bats': cols[17], 'OBP': cols[18], 'SLG': cols[19], 'OPS': cols[20], 'OPS+': cols[21]}]
+        data_dict[key] = [{'Year': cols[0], 'Age': cols[1], 'Team': cols[2], 'League': cols[3], 'Games Played': cols[4], 'Plate Appearance': cols[5], 'At Bat': cols[6], ' Runs Scored': cols[7], 'Hits': cols[8], 'Doubles': cols[9], 'Triples': cols[10], 'Homeruns': cols[11], 'RBI': cols[12], 'Stolen Bases': cols[13], 'Caught Stealing': cols[14], 'BB/Walks': cols[15], 'Strikeouts': cols[16], 'Hits/At Bats': cols[17], 'OBP': cols[18], 'SLG': cols[19], 'OPS': cols[20]}]
 
+
+    #data = []
 
     # for row in data_rows:
     #     cols = row.find_all(['th', 'td'])
